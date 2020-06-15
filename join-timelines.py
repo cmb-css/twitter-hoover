8b2b89d0ea8ccd2ef0494026aa000f58afaa0283
Telmo Menezes <telmo@telmomenezes.net>
2020-06-15 13:08:11 +0200

    join timelines

--- join-timelines.py
+++ join-timelines.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     dir_names = glob.glob('timelines/*')
     n = len(dir_names)
 
-    with open('{}.json'.format(YEAR_MONTH), 'w') as output_file:
+    with open('{}.json'.format(YEAR_MONTH), 'wt') as output_file:
         for i, dir_name in enumerate(dir_names):
             print('[{}/{}] {}'.format(i, n, dir_name))
             user_id = os.path.basename(dir_name)
             file_name = 'timelines/{}/{}.json.gz'.format(user_id, YEAR_MONTH)
             try:
-                with gzip.open(file_name, 'r') as f:
+                with gzip.open(file_name, 'rt') as f:
                     for line in f.readlines():
                         output_file.write(line)
             except FileNotFoundError:
