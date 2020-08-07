7f2b700787eee1e931ebb883ae8477baba7f138c
Telmo Menezes <telmo@telmomenezes.net>
2020-08-07 10:21:18 +0200

    lowering hydrate requests rate

--- hoover/hydrate.py
+++ hoover/hydrate.py
@@ -28,7 +28,7 @@ def json_split(json_str):
 
 class Hydrate(RateControl):
     def __init__(self, infile, outfile, errfile, key_file, auth_file):
-        super().__init__(rate_limit=800)
+        super().__init__(rate_limit=500)
         self.infile = infile
         self.outfile = outfile
         self.errfile = errfile
