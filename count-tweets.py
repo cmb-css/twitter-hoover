976b4ec0a8c038bd1c61453bd476c0205a7b595f
Telmo Menezes <telmo@telmomenezes.net>
2021-01-27 16:05:50 +0100

    count-tweets script

---
+++ count-tweets.py
@@ -0,0 +1,20 @@
+import gzip
+from pathlib import Path
+
+
+if __name__ == '__main__':
+    p = Path('timelines').glob('**/*')
+    dirs = [x for x in p if x.is_dir()]
+
+    count = 0
+    for d in dirs:
+        print('directory: {}'.format(d))
+        p = Path(d).glob('**/*')
+        files = [x for x in p 
+                 if x.is_file() and 'hydated' not in x and '.json.gz' in x]
+        for file in files:
+            print('file: {}'.format(file))
+            with gzip.open(file, 'rt') as f:
+                for line in f:
+                    count += 1
+            print('count: {}'.format(count))
