e130cdd81b73970d6014fcc50d094ab80f89c01e
Telmo Menezes <telmo@telmomenezes.net>
2021-01-27 16:10:10 +0100

    count-tweets script

--- count-tweets.py
+++ count-tweets.py
@@ -3,15 +3,14 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    p = Path('timelines').glob('**/*')
+    p = Path('timelines').glob('*')
     dirs = [x for x in p if x.is_dir()]
 
     count = 0
     for d in dirs:
         print('directory: {}'.format(d))
-        p = Path(d).glob('**/*')
-        files = [x for x in p 
-                 if x.is_file() and 'hydated' not in x and '.json.gz' in x]
+        p = Path(d).glob('*.json.gz')
+        files = [x for x in p if x.is_file() and 'hydated' not in x]
         for file in files:
             print('file: {}'.format(file))
             with gzip.open(file, 'rt') as f:
