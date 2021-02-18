6ea1034b9abf90c0f1c4285f354c265db1e6a6f1
Telmo Menezes <telmo@telmomenezes.net>
2021-02-18 15:45:26 +0100

    lang-users

--- lang-users.py
+++ lang-users.py
@@ -49,7 +49,8 @@ class LangUsers(object):
             if user_tweets > 0:
                 percents = list((lang, count / user_tweets)
                                 for lang, count in lang_counter.most_common())
-                if list[0][0] == self.lang and list[0][1] >= 15.0:
+                if (lang_counter[0][0] == self.lang and
+                        lang_counter[0][1] >= 15.0):
                     self.lang_users.append(user_id)
                 print(' | '.join(['{} {:.0f}%'.format(lang, percent)
                       for lang, percent in percents]))
