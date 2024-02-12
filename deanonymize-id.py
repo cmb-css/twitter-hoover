63d8e0d5a0489361cd5fec21a524ec609acc4655
Telmo Menezes <telmo@telmomenezes.net>
2024-02-12 18:09:17 +0100

    deanonymize-id

---
+++ deanonymize-id.py
@@ -0,0 +1,13 @@
+import argparse
+
+from hoover.anon.decrypt import deanonymize
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', type=str, help='id to anonymize', default=None)
+    parser.add_argument("--anon_db_folder_path", type=str, help="Path to anon DB.", default='/home/socsemics/anon')
+    args = parser.parse_args()
+
+    print(f'user id: {args.id}')
+    print('result: {}'.format(deanonymize(args.id, args.anon_db_folder_path)))
